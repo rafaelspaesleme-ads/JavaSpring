ba82b42f80f9ddf03ad14bf23d5cb590d2440770
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2019-01-22 22:51:13 -0200

    Criando metodo para apresentar dados em lista no CategoriaResource

--- src/main/java/br/com/avantews/resource/CategoriaResource.java
+++ src/main/java/br/com/avantews/resource/CategoriaResource.java
@@ -1,17 +1,30 @@
 package br.com.avantews.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.avantews.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
+
 	//Apresenta dados para o final user!
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest yes found!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Service");
+		Categoria cat2 = new Categoria(2, "Client");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
