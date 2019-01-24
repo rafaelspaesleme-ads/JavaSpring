9f390878a49616842c981e4a02157be0d4677be2
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2019-01-23 23:23:17 -0200

    Inserindo JsonManagedReference em Categoria e JsonbackRef em Produtos e criando exception em caso de objeto não enccontrado em service

--- src/main/java/br/com/avantews/domain/Produto.java
+++ src/main/java/br/com/avantews/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -22,7 +24,8 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 	
-	//Criando taabela de relacionamento muitos para muitos
+	//Criando taabela de relacionamento muitos para muitos^
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "produto_categoria",
 				joinColumns = @JoinColumn(name = "fk_produto"),
--- src/main/java/br/com/avantews/services/CategoriaService.java
+++ src/main/java/br/com/avantews/services/CategoriaService.java
@@ -2,6 +2,7 @@ package br.com.avantews.services;
 
 import java.util.Optional;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +15,11 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	//tratamento de objetos da base de dados
 	public Categoria buscar(Integer id) {
 				Optional<Categoria> objeto = categoriaRepository.findById(id);
-				return objeto.orElse(null);
+				return objeto.orElseThrow(() -> new ObjectNotFoundException
+						("Objeto não encontrato na base de dados!", Categoria.class.getName()));
 	}
 	
 }
