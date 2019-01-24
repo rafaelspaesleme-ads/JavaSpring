6dfff7f8592a645980ab6425584792c61e242779
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2019-01-23 22:26:59 -0200

    Utilizando metodo  saveAll para salvar dados  de categoria no banco

--- src/main/java/br/com/avantews/JavaSpringApplication.java
+++ src/main/java/br/com/avantews/JavaSpringApplication.java
@@ -1,13 +1,34 @@
 package br.com.avantews;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class JavaSpringApplication {
+import br.com.avantews.domain.Categoria;
+import br.com.avantews.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class JavaSpringApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
