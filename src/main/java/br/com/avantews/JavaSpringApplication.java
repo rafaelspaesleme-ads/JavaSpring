758be956531e00e1dd72dafd9e36ab987ea82279
Rafael <rafaelspaesleme.ads@gmail.com>
2019-01-31 09:29:48 -0200

    create conection with database of pedidos and pagamentos

--- src/main/java/br/com/avantews/JavaSpringApplication.java
+++ src/main/java/br/com/avantews/JavaSpringApplication.java
@@ -2,26 +2,25 @@ package br.com.avantews;
 
 //My projects they are in https://bit.ly/2GglPYK
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Date;
 
+import br.com.avantews.domain.*;
+import br.com.avantews.domain.enums.EstadoPagamento;
+import br.com.avantews.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.avantews.domain.Categoria;
-import br.com.avantews.domain.Cidade;
-import br.com.avantews.domain.Cliente;
-import br.com.avantews.domain.Endereco;
-import br.com.avantews.domain.Estado;
-import br.com.avantews.domain.Produto;
 import br.com.avantews.domain.enums.TipoCliente;
-import br.com.avantews.repositories.CategoriaRepository;
-import br.com.avantews.repositories.CidadeRepository;
-import br.com.avantews.repositories.ClienteRepository;
-import br.com.avantews.repositories.EnderecoRepository;
-import br.com.avantews.repositories.EstadoRepository;
-import br.com.avantews.repositories.ProdutoRepository;
+
+import static java.time.LocalDate.now;
 
 @SpringBootApplication
 public class JavaSpringApplication implements CommandLineRunner {
@@ -43,6 +42,12 @@ public class JavaSpringApplication implements CommandLineRunner {
     
     @Autowired
     private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
     
     public static void main(String[] args) {
         SpringApplication.run(JavaSpringApplication.class, args);
@@ -70,7 +75,18 @@ public class JavaSpringApplication implements CommandLineRunner {
         Endereco end1 = new Endereco(null, "Rua Flores da Junqueira", "300", "Apto 203", "Jardim Botanico", "25715363", cli1, cid1);
         Endereco end2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, cid2);
         cli1.getEnderecos().addAll(Arrays.asList(end1, end2));
-                
+
+        SimpleDateFormat data = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+
+        Pedido ped1 = new Pedido(null, data.parse("31/01/2019 12:00"), cli1, end1);
+        Pedido ped2 = new Pedido(null, data.parse("01/02/2019 11:00"), cli1, end2);
+
+        Pagamento pgto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+        ped1.setPagamento(pgto1);
+        Pagamento pgto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, data.parse("28/01/2019 14:00"), null);
+        ped2.setPagamento(pgto2);
+
+
         //Associando categorias ao tipo de produtos correspondente.
         cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
         cat2.getProdutos().addAll(Arrays.asList(p2));
@@ -81,6 +97,9 @@ public class JavaSpringApplication implements CommandLineRunner {
         p1.getCategorias().addAll(Arrays.asList(cat1));
         p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
         p3.getCategorias().addAll(Arrays.asList(cat1));
+
+        //Associando clientes a seus pedidos
+        cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 
         //Salvando categorias na base de dadoss
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
@@ -94,6 +113,10 @@ public class JavaSpringApplication implements CommandLineRunner {
         clienteRepository.saveAll(Arrays.asList(cli1));
         //Salvando endereços na base de dados
         enderecoRepository.saveAll(Arrays.asList(end1, end2));
+        //Salvando pedidos na base de dados
+        pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+        //Salvando pagamentos na base de dados
+        pagamentoRepository.saveAll(Arrays.asList(pgto1, pgto2));
                 
     }
 }
