11a5bc699c3f59e44b980f1f457ac83e7905f25d
Rafael <rafaelspaesleme.ads@gmail.com>
2019-01-31 14:23:57 -0200

    Create class repositories in ItemPedido

--- src/main/java/br/com/avantews/repositories/ItemPedidoRepository.java
+++ src/main/java/br/com/avantews/repositories/ItemPedidoRepository.java
@@ -1,4 +1,9 @@
 package br.com.avantews.repositories;
 
-public interface ItemPedidoRepository {
+import br.com.avantews.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
 }
